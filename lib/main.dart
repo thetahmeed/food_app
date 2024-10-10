9ad5b6c76acf90c0b1245909bb0e24bf8a859767
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2024-10-11 00:25:20 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
